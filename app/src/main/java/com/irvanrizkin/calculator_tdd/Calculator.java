442f51834302b16b768775bd048de42522e1499e
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2020-11-07 23:52:48 +0700

    fix indentation and spacing : GREEN

--- app/src/main/java/com/irvanrizkin/calculator_tdd/Calculator.java
+++ app/src/main/java/com/irvanrizkin/calculator_tdd/Calculator.java
@@ -2,32 +2,33 @@ package com.irvanrizkin.calculator_tdd;
 
 public class Calculator {
   double result = 0;
+
   @Override
   public String toString() {
     return String.valueOf(result);
   }
 
-  public void add(int a, int b){
+  public void add(int a, int b) {
     result = a + b;
   }
 
-  public void subtract(int a, int b){
+  public void subtract(int a, int b) {
     result = a - b;
   }
 
-  public void multiply(int a, int b){
+  public void multiply(int a, int b) {
     result = a * b;
   }
 
-  public void divide(int a, int b){
-    if (b == 0){
+  public void divide(int a, int b) {
+    if (b == 0) {
       cancel();
       return;
     }
     result = a / b;
   }
 
-  public void cancel(){
+  public void cancel() {
     result = 0;
   }
 }
--- app/src/test/java/com/irvanrizkin/calculator_tdd/CalculatorTest.java
+++ app/src/test/java/com/irvanrizkin/calculator_tdd/CalculatorTest.java
@@ -5,48 +5,48 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
   @Test
-  public void whenToStringReturnInitialResult(){
+  public void whenToStringReturnInitialResult() {
     Calculator calculator = new Calculator();
-    assertEquals("0.0",calculator.toString());
+    assertEquals("0.0", calculator.toString());
   }
 
   @Test
-  public void whenAddNumbersReturnNumber(){
+  public void whenAddNumbersReturnNumber() {
     Calculator calculator = new Calculator();
     calculator.add(5,2);
     assertEquals("7.0", calculator.toString());
   }
 
   @Test
-  public void whenSubtractNumbersReturnNumber(){
+  public void whenSubtractNumbersReturnNumber() {
     Calculator calculator = new Calculator();
     calculator.subtract(5,2);
     assertEquals("3.0", calculator.toString());
   }
 
   @Test
-  public void whenMultiplyNumbersReturnNumber(){
+  public void whenMultiplyNumbersReturnNumber() {
     Calculator calculator = new Calculator();
     calculator.multiply(4,2);
-    assertEquals("8.0",calculator.toString());
+    assertEquals("8.0", calculator.toString());
   }
 
   @Test
-  public void whenDivisionNumbersReturnNumbers(){
+  public void whenDivisionNumbersReturnNumbers() {
     Calculator calculator = new Calculator();
     calculator.divide(4,2);
     assertEquals("2.0", calculator.toString());
   }
 
   @Test
-  public void whenDivisionByZeroReturnZero(){
+  public void whenDivisionByZeroReturnZero() {
     Calculator calculator = new Calculator();
     calculator.divide(4,0);
     assertEquals("0.0", calculator.toString());
   }
 
   @Test
-  public void whenCancelReturnZero(){
+  public void whenCancelReturnZero() {
     Calculator calculator = new Calculator();
     calculator.cancel();
     assertEquals("0.0", calculator.toString());
